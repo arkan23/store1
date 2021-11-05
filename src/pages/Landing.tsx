4bad54047eea408ad2c2bd2feb7fa9158074da92
Ark <arkan23a@yandex.ru>
2021-11-05 15:32:25 +0300

    Fix webpack

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -7,12 +7,26 @@ const Landing = observer(() => {
     const appStore = useContext(AppContext);
     return (
         <div className={'LandingWrapper'}>
+            <div className={'container-fluid Landing-topBar'}>
+                <div className='row'>
+                    <div className='col-12'>
+                        <nav className="navbar navbar-light bg-light">
+                            <div className="container">
+                                <a className="navbar-brand" href="#">
+                                    <img src="/public/img/logo/logo.png" alt=""
+                                         width="30" height="24"/>
+                                </a>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
             <div className='container'>
                 <div className='row'>
                     <div className='col-4'>
             Одна из трёх колонок
                     </div>
-                    <div className='col-4'>Одна из трёх колонок</div>
+                    <div className='col-4 '>Одна из трёх колонок</div>
                     <div className='col-4'>Одна из трёх колонок</div>
                     <div className='col-4'>Одна из трёх колонок</div>
                     <div className='col-4'>Одна из трёх колонок</div>
--- webpack.config.js
+++ webpack.config.js
@@ -6,66 +6,94 @@ const {ProvidePlugin, DefinePlugin} = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintWebpackPlugin = require('eslint-webpack-plugin');
 const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-module.exports = {
-    mode: 'development',
-    entry: {
-        app: path.join(__dirname, 'src', 'index.tsx')
-    },
-    target: 'web',
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-            '@types': path.resolve(__dirname, 'src/interface'),
-            '@store': path.resolve(__dirname, 'src/store/index'),
-            '@util': path.resolve(__dirname, 'src/common/util')
+module.exports = (env) => {
+    console.log(env)
+    return {
+        mode: env.WEBPACK_SERVE ? 'development' : 'production',
+        entry: {
+            bootstrap: path.join(__dirname, 'src/style/bootstrap', 'bootstrap.bundle.min.js'),
+            app: path.join(__dirname, 'src', 'index.tsx')
         },
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)?$/,
-                use: 'ts-loader',
-                exclude: '/node_modules/'
+        // FIXME
+        performance: {
+            hints: false,
+            maxEntrypointSize: 512000,
+            maxAssetSize: 512000
+        },
+        target: 'web',
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+                '@types': path.resolve(__dirname, 'src/interface'),
+                '@store': path.resolve(__dirname, 'src/store/index'),
+                '@util': path.resolve(__dirname, 'src/common/util')
             },
-            {
-                test: /\.(s(a|c)ss)$/,
-                use: ['style-loader','css-loader', 'sass-loader']
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|svg|jpg|png)$/,
-                use: {
-                    loader: 'url-loader',
+            extensions: ['.ts', '.tsx', '.js']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx)?$/,
+                    use: 'ts-loader',
+                    exclude: '/node_modules/'
                 },
-            },
-        ],
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'public', 'index.html')
-        }),
-        new ESLintWebpackPlugin({
-            extensions: ['ts']
-        }),
-        // new BundleAnalyzerPlugin(),
-        new DefinePlugin({
-            'process.env': JSON.stringify(dotenv.config().parsed)
-        }),
-        new ProvidePlugin({
-            process: 'process/browser',
-        }),
-    ],
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'public'),
+                {
+                    test: /\.(s(a|c)ss)$/,
+                    use: ['style-loader','css-loader', 'sass-loader']
+                },
+                {
+                    test: /\.(svg|jpg|png)$/,
+                    type: "asset/resource"
+                },
+                {
+                    test: /\.(woff|woff2|eot|otf|ttf)$/,
+                    use: {
+                        loader: 'url-loader',
+                    },
+                },
+            ],
         },
-        hot: true,
-        port: 9000,
-        historyApiFallback: true,
-    },
+        output: {
+            filename: '[name].bundle.[contenthash].js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, './', 'index.html')
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: path.join(__dirname, 'public'),
+                        to: 'public',
+                        globOptions: {
+                            ignore: ['*.DS_Store'],
+                        },
+                    },
+                ],
+            }),
+            new ESLintWebpackPlugin({
+                extensions: ['ts']
+            }),
+            // new BundleAnalyzerPlugin(),
+            new DefinePlugin({
+                'process.env': JSON.stringify(dotenv.config().parsed)
+            }),
+            new ProvidePlugin({
+                process: 'process/browser',
+            }),
+        ],
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'public'),
+            },
+            hot: true,
+            port: 9000,
+            historyApiFallback: true,
+        },
+    }
 }
